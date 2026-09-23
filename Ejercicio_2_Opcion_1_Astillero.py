35f9c6bb56a889b66549d224ca2de0279c353458
jastsa2 <jastsa2@hotmail.com>
2026-09-23 16:15:18 +0200

    Ejercicio 2
    Buenas Sauca. Me salieron dos ejercicios con mismo resultado, primero me salió la opción 2 que me parecía chapucera, luego le di una vuelta a if_in y parece que sale algo mas pulcro. Ya me comentaras tu que tal lo ves. Un saludo y muchas gracias

---
+++ Ejercicio_2_Opcion_1_Astillero.py
@@ -0,0 +1,11 @@
+#Saludos Sauca. Hice dos versiones del ejercicio las dos funcionan
+#por igual pero me gustaria feedback de cual ves mejor y por supuesto que cambiarias.
+#Muchas gracias y un abrazo.
+
+number = 1
+def tabla(number):
+    for number in range(1,11):
+         print ("1 x", number, "=", number, end=" ")
+
+tabla(number)
+
---
+++ Ejercicio_2_Opcion_2_Astillero.py
@@ -0,0 +1,13 @@
+def tabla(number):
+    print ("1 x", number, "=", number, end=" ")
+
+tabla(1)
+tabla(2)
+tabla(3)
+tabla(4)
+tabla(5)
+tabla(6)
+tabla(7)
+tabla(8)
+tabla(9)
+tabla(10)
